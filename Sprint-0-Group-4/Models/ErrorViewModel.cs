1dd1a5244b0178bd1acb67428fcba09f8f9d4234
Jacob Baker <baker99jab@gmail.com>
2026-01-28 14:52:06 -0500

    Add project files.

---
+++ Sprint-0-Group-4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sprint_0_Group_4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
